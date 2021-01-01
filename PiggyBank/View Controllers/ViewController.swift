a8985bb5f234eeb400f5187c0f04b713f5a940b2
kaylawang05 <kayla.y.wang@gmail.com>
2021-01-01 14:59:40 -0500

    struggling

--- PiggyBank/View Controllers/ViewController.swift
+++ PiggyBank/View Controllers/ViewController.swift
@@ -8,14 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    //let goal = GoalView()
     let titleLable = UILabel()
-    
-    
-    let container = UIView()
-    
-    let start = UIButton()
-    let pause = UIButton()
     
 
     override func viewDidLoad() {
@@ -25,40 +19,64 @@ class ViewController: UIViewController {
         
         
         self.view.addSubview(titleLable)
-        titleLable.text = "Hello World"
-        titleLable.textAlignment = .center
-        titleLable.constrain(height: 20)
+        titleLable.backgroundColor = UIColor.red
+
         titleLable.constrain(to: self.view, leadingInset: 20, trailingInset: -20, centerYInset: 0, centerXInset: 0)
 //        titleLable.backgroundColor = UIColor.blue
         
-        
-        
-        container.addSubview(start)
-        container.addSubview(pause)
-        
-        
-        self.view.addSubview(start)
-        self.view.addSubview(pause)
-        
-        start.setTitle("start", for: .normal)
-        pause.setTitle("pause", for: .normal)
-        
-        start.constrain(against: pause, trailingInset: -50)
-        start.constrain(to: pause, widthInset: 0)
-        start.constrain(to: self.view, leadingInset: 50, centerYInset: 0)
-        start.constrain(height: 30)
-        start.backgroundColor = UIColor.blue
-        
-        pause.constrain(to: self.view, trailingInset: -50, centerYInset: 0)
-        pause.constrain(height: 30)
-        pause.backgroundColor = UIColor.red
-        
-        
-        
-        
-        
+
     }
+    /*
+    let label = UILabel()
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        self.view.addSubview(label)
+        label.text = "Hello World"
+    }
+ */
+    /*
+    let piggyImage = UIImageView()
+    let container = UIView()
+    let goalName = UILabel()
+    let progressNumberLabel = UILabel()
+    let progressBar = UILabel()
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //contain.addSubview(goal)
+        //goal.createGoal()
+        
+        self.view.addSubview(piggyImage)
+        self.view.addSubview(container)
+        container.addSubview(goalName)
+        container.addSubview(progressNumberLabel)
+        container.addSubview(progressBar)
+        
+        piggyImage.backgroundColor = UIColor.green;
+        goalName.backgroundColor = UIColor.red;
+        progressNumberLabel.backgroundColor = UIColor.yellow;
+        progressBar.backgroundColor = UIColor.blue;
+        
+        //piggyImage.constrain(to: self, topInset: 0, bottomInset: 0, leadingInset: 0, centerYInset: 0)
+        piggyImage.constrain(to: container, widthInset: 0)
+        goalName.textAlignment = .center
+        progressNumberLabel.textAlignment = .center
+        //container.constrain(to: self, topInset: 0, bottomInset: 0, trailingInset: 0, centerYInset: 0)
+        container.constrain(to: piggyImage, leadingInset: 10)
+        
+        //constraining widths and heights
+        goalName.constrain(to: progressNumberLabel, widthInset: 0)
+        goalName.constrain(to: progressBar, heightInset: 0)
+        progressNumberLabel.constrain(to: progressBar, heightInset: 0)
+        
+        goalName.constrain(to: container, topInset: 0, leadingInset: 0)
+        goalName.constrain(to: progressNumberLabel, trailingInset: 0)
+        progressNumberLabel.constrain(to: container, topInset: 0, trailingInset: 0)
+        progressBar.constrain(to: container, bottomInset: 0, leadingInset: 0, trailingInset: 0);
+    
+    }
+ */
 }
 
--- PiggyBank/Views/GoalView.swift
+++ PiggyBank/Views/GoalView.swift
@@ -11,6 +11,11 @@ import UIKit
 class GoalView: UIView {
     
     // Varbales
+    let piggyImage = UIImageView()
+    let container = UIView()
+    let goalName = UILabel()
+    let progressNumberLabel = UILabel()
+    let progressBar = UILabel()
     
     init() {
         super.init(frame: .zero)
@@ -21,6 +26,36 @@ class GoalView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func createGoal() {
+        self.constrain(height: 100)
+        self.backgroundColor = UIColor.white
+        self.addSubview(piggyImage)
+        self.addSubview(container)
+        container.addSubview(goalName)
+        container.addSubview(progressNumberLabel)
+        container.addSubview(progressBar)
+        
+        piggyImage.backgroundColor = UIColor.green;
+        goalName.backgroundColor = UIColor.red;
+        progressNumberLabel.backgroundColor = UIColor.yellow;
+        progressBar.backgroundColor = UIColor.blue;
+        
+        //piggyImage.constrain(to: self, topInset: 0, bottomInset: 0, leadingInset: 0, centerYInset: 0)
+        piggyImage.constrain(to: container, widthInset: 0)
+        goalName.textAlignment = .center
+        progressNumberLabel.textAlignment = .center
+        //container.constrain(to: self, topInset: 0, bottomInset: 0, trailingInset: 0, centerYInset: 0)
+        container.constrain(to: piggyImage, leadingInset: 10)
+        
+        //constraining widths and heights
+        goalName.constrain(to: progressNumberLabel, widthInset: 0)
+        progressNumberLabel.constrain(to: goalName, heightInset: 0)
+        
+        goalName.constrain(to: container, topInset: 0, leadingInset: 0)
+        goalName.constrain(to: progressNumberLabel, trailingInset: 0)
+        progressNumberLabel.constrain(to: container, topInset: 0, trailingInset: 0)
+        progressBar.constrain(to: container, bottomInset: 0, leadingInset: 0, trailingInset: 0);
+    }
     
 //    func updateData(with data: CustomDataObject) {
 //
